d6ec8350154250ba4e03519818984389cf1f9c3a
RogerSelwyn <rogers@selwyn.org.uk>
2024-09-17 18:15:14 +0100

    test: Add more robust testing for multi property

--- tests/test_7_updates.py
+++ tests/test_7_updates.py
@@ -5,7 +5,7 @@ from datetime import date
 from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from .conftest import HomelinkMockConfigEntry, standard_mocks
 from .helpers.utils import create_mock
@@ -13,17 +13,23 @@ from .helpers.utils import create_mock
 
 async def test_add_property(
     hass: HomeAssistant,
-    setup_base_integration: None,
-    base_config_entry: HomelinkMockConfigEntry,
+    setup_insight_integration: None,
+    insight_config_entry: HomelinkMockConfigEntry,
     device_registry: dr.DeviceRegistry,
+    entity_registry: er.EntityRegistry,
     aioclient_mock: AiohttpClientMocker,
 ):
     """Test addition of new property."""
-    coordinator = base_config_entry.runtime_data.coordinator
+    coordinator = insight_config_entry.runtime_data.coordinator
     devices = device_registry.devices.get_devices_for_config_entry_id(
-        base_config_entry.entry_id
+        insight_config_entry.entry_id
     )
     assert len(devices) == 10
+
+    entities = er.async_entries_for_config_entry(
+        entity_registry, insight_config_entry.entry_id
+    )
+    assert len(entities) == 41
 
     aioclient_mock.clear_requests()
     add_property_mocks(aioclient_mock)
@@ -31,9 +37,14 @@ async def test_add_property(
     await coordinator.async_refresh()
     await hass.async_block_till_done()
     devices = device_registry.devices.get_devices_for_config_entry_id(
-        base_config_entry.entry_id
+        insight_config_entry.entry_id
     )
-    assert len(devices) == 14
+    assert len(devices) == 16
+
+    entities = er.async_entries_for_config_entry(
+        entity_registry, insight_config_entry.entry_id
+    )
+    assert len(entities) == 56
 
 
 async def test_add_device(
@@ -41,6 +52,7 @@ async def test_add_device(
     setup_base_integration: None,
     base_config_entry: HomelinkMockConfigEntry,
     device_registry: dr.DeviceRegistry,
+    entity_registry: er.EntityRegistry,
     aioclient_mock: AiohttpClientMocker,
 ):
     """Test addition of new device."""
@@ -50,6 +62,11 @@ async def test_add_device(
     )
     assert len(devices) == 10
 
+    entities = er.async_entries_for_config_entry(
+        entity_registry, base_config_entry.entry_id
+    )
+    assert len(entities) == 27
+
     aioclient_mock.clear_requests()
     add_device_mocks(aioclient_mock)
 
@@ -59,6 +76,11 @@ async def test_add_device(
         base_config_entry.entry_id
     )
     assert len(devices) == 11
+
+    entities = er.async_entries_for_config_entry(
+        entity_registry, base_config_entry.entry_id
+    )
+    assert len(entities) == 30
 
 
 async def test_delete_device(
@@ -66,6 +88,7 @@ async def test_delete_device(
     setup_base_integration: None,
     base_config_entry: HomelinkMockConfigEntry,
     device_registry: dr.DeviceRegistry,
+    entity_registry: er.EntityRegistry,
     aioclient_mock: AiohttpClientMocker,
 ):
     """Test deletion of device."""
@@ -80,6 +103,11 @@ async def test_delete_device(
     )
     assert len(devices) == 11
 
+    entities = er.async_entries_for_config_entry(
+        entity_registry, base_config_entry.entry_id
+    )
+    assert len(entities) == 30
+
     aioclient_mock.clear_requests()
     standard_mocks(aioclient_mock)
     await coordinator.async_refresh()
@@ -89,34 +117,50 @@ async def test_delete_device(
     )
     assert len(devices) == 10
 
+    entities = er.async_entries_for_config_entry(
+        entity_registry, base_config_entry.entry_id
+    )
+    assert len(entities) == 27
+
 
 async def test_delete_property(
     hass: HomeAssistant,
-    setup_base_integration: None,
-    base_config_entry: HomelinkMockConfigEntry,
+    setup_insight_integration: None,
+    insight_config_entry: HomelinkMockConfigEntry,
     device_registry: dr.DeviceRegistry,
+    entity_registry: er.EntityRegistry,
     aioclient_mock: AiohttpClientMocker,
 ):
     """Test deletion of device."""
-    coordinator = base_config_entry.runtime_data.coordinator
+    coordinator = insight_config_entry.runtime_data.coordinator
 
     aioclient_mock.clear_requests()
     add_property_mocks(aioclient_mock)
     await coordinator.async_refresh()
     await hass.async_block_till_done()
     devices = device_registry.devices.get_devices_for_config_entry_id(
-        base_config_entry.entry_id
+        insight_config_entry.entry_id
     )
-    assert len(devices) == 14
+    assert len(devices) == 16
+
+    entities = er.async_entries_for_config_entry(
+        entity_registry, insight_config_entry.entry_id
+    )
+    assert len(entities) == 56
 
     aioclient_mock.clear_requests()
     standard_mocks(aioclient_mock)
     await coordinator.async_refresh()
     await hass.async_block_till_done()
     devices = device_registry.devices.get_devices_for_config_entry_id(
-        base_config_entry.entry_id
+        insight_config_entry.entry_id
     )
     assert len(devices) == 10
+
+    entities = er.async_entries_for_config_entry(
+        entity_registry, insight_config_entry.entry_id
+    )
+    assert len(entities) == 41
 
 
 def add_property_mocks(
